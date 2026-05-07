e98aa3eda7a00df13f5f005e5f93826329c68ce0
Christine Tham <chris.tham@hellotham.com>
2026-05-07 13:31:20 +1000

    feat(config): Remove Alpine.js integration from astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,8 @@ import { defineConfig } from 'astro/config'
 import UnoCSS from 'unocss/astro'
 import sitemap from '@astrojs/sitemap'
 
-import alpinejs from '@astrojs/alpinejs'
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://astro-spotlite.netlify.app',
-  integrations: [UnoCSS(), sitemap(), alpinejs()]
+  integrations: [UnoCSS(), sitemap()]
 })
